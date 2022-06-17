1c3d39aaa56fe7acaf8cf0f196573a2aac3a15db
ppapisdb <24455863+ppapisdb@users.noreply.github.com>
2022-06-16 23:18:25 -0400

    Update upload_video.py

--- upload_video.py
+++ upload_video.py
@@ -130,7 +130,7 @@ def resumable_upload(insert_request):
   retry = 0
   while response is None:
     try:
-      #print("Uploading file...")
+      print()
       status, response = insert_request.next_chunk()
       if response is not None:
         if 'id' in response:
